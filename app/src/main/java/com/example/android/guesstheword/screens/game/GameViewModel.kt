eed380cd755df42ce49457555f9838531fc494af
howoanling <howl-sm17@student.tarc.edu.my>
2019-12-03 14:05:20 +0800

    GueesGameWorld

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,81 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    // The current word
+    var word = MutableLiveData<String>()
+    // The current score
+    var score = MutableLiveData<Int>()
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    init {
+        resetList()
+        nextWord()
+        word.value = ""
+        score.value = 0
+        Log.i("GameViewModel", "GameViewModel created!")
+    }
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        if (!wordList.isEmpty()) {
+            //Select and remove a word from the list
+            word.value = wordList.removeAt(0)
+        }
+
+    }
+    /** Methods for buttons presses **/
+    fun onSkip() {
+        if (!wordList.isEmpty()) {
+            score.value = (score.value)?.minus(1)
+        }
+        nextWord()
+    }
+
+    fun onCorrect() {
+        if (!wordList.isEmpty()) {
+            score.value = (score.value)?.plus(1)
+        }
+        nextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+}
